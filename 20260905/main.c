623544777ce2254b43e03665b7cbfd7a7eedd5e9
anonline <uerik26@gmail.com>
2026-09-05 09:58:08 +0200

    elsó óra példák

--- 20260905/main.c
+++ 20260905/main.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
 
 int main() {
+    //Első példa
     printf("Hello, World!\n");
+
+    //Második példa
+    for (int idxI = 0; idxI < 10; idxI++) {
+        printf("idxI = %d\n", idxI);
+    }
+
+    //Harmadik példa
+    float result;
+
+    printf("result=");
+    scanf("%f", &result);
+
+    if(result > 50) {
+        printf("Passed\n");
+    } else {
+        printf("Failed\n");
+    }
+    
     return 0;
 }
